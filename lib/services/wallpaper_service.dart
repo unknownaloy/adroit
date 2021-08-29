6618b606fb336a571cd0357f7c76e72fc9e78280
unknownaloy <niceju4u@gmail.com>
2021-08-29 11:12:04 +0100

    Added the service to fetch wallpaper data data from unisplash api and data parsing

--- lib/services/wallpaper_service.dart
+++ lib/services/wallpaper_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:adroit/api_keys/api.dart';
 import 'package:adroit/models/wallpaper.dart';
@@ -7,23 +8,32 @@ import 'package:http/http.dart' as http;
 class WallpaperService {
   final String baseUrl = "https://api.unsplash.com/photos/";
 
-  Future<Wallpaper?> getListOfPhotos([int page = 1]) async {
-    /// TODO: Wrap in a try-catch block
-    Map<String, String> queryParams = {
-      "client_id": clientId,
-      "page": page.toString(),
-    };
+  Future<Wallpaper> getListOfPhotos([int page = 1]) async {
+    try {
+      Map<String, String> queryParams = {
+        "client_id": clientId,
+        "page": page.toString(),
+      };
 
-    var uri = Uri.https("api.unsplash.com", "/photos/", queryParams);
+      var uri = Uri.https("api.unsplash.com", "/photos/", queryParams);
 
-    http.Response response = await http.get(uri);
+      http.Response response = await http.get(uri);
 
-    if (response.statusCode == 200) {
-      final data = response.body;
+      if (response.statusCode == 200) {
+        final data = response.body;
 
-      final decodedData = jsonDecode(data);
-    } else {
-      print("WALLPAPER SERVICE => getListOfPhotos === Something went wrong");
+        final decodedData = jsonDecode(data);
+
+        Wallpaper wallpaper = Wallpaper.fromJson(decodedData);
+
+        return wallpaper;
+      } else {
+        throw Exception("An error occurred while fetching data");
+      }
+    } on SocketException {
+      throw Exception("No internet connection");
+    } catch (e) {
+      throw Exception("Something went wrong please try again");
     }
   }
 }
